83d6353b4fb0d0d5bafbf6a3de7a8a8aefffdf43
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-11-04 16:19:17 +0900

    세팅중

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,25 @@
+// NEXT_DEFAULT
 import type {Metadata} from "next";
+
+// components
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
+
+// GA & log & performance
+import {GoogleAnalytics, GoogleTagManager} from "@next/third-parties/google";
+
+// fonts
 import localFont from "next/font/local";
+
+// styles
 import "./globals.css";
+
+// provider
+import {ClientProvider} from "@/components/provider/ClientProvider";
+import {ThemeProvider} from "@/components/provider/theme-provider";
+
+// third-party
+import NextTopLoader from "nextjs-toploader";
 
 const geistSans = localFont({
   src: "../assets/fonts/GeistVF.woff",
@@ -27,7 +46,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <NextTopLoader color="#16a34a" shadow="0 0 10px #16a34a,0 0 5px #16a34a" />
+        <ClientProvider>
+          <ThemeProvider attribute="class" defaultTheme="system">
+            <div id="wrap">
+              <Header />
+              <main id="content" role="main">
+                {children}
+              </main>
+              <Footer />
+            </div>
+          </ThemeProvider>
+        </ClientProvider>
+        <GoogleAnalytics gaId="G-내계정키 환경변수로 넣을꺼임" />
+        <GoogleTagManager gtmId="G-내계정키 환경변수로 넣을꺼임" />
+      </body>
     </html>
   );
 }
